f149cccd326026be6981f1ba504cf2920898a479
Liudmila <liudmilagubanova18@gmail.com>
2023-07-10 22:37:50 +0400

    add changes

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,15 +1,9 @@
 <!DOCTYPE html>
-<!-- Хранится в resources/views/about.blade.php -->
 
 @extends('layouts.app')
 
 <!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
 @section('content')
-    <h1>Список статей</h1>
-    @foreach ($articles as $article)
-        <h2>{{$article->name}}</h2>
-        <!--{{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
-        {{-- Используется для очень длинных текстов, которые нужно сократить --}}-->
-        <div>{{Str::limit($article->body, 200)}}</div>
-    @endforeach
+    <h1>{{$article->name}}</h1>
+    <div>{{$article->body}}</div>
 @endsection
